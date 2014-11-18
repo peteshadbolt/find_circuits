030125ea62d6e42f4ca235afeccf31fcc1b029cd
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-11-18 16:44:09 +0000

    Added circuit viewer - bit of a hack for now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import linear_optics as lo
 import numpy as np
 from collections import defaultdict
+import itertools as it
+import json
 
 p2=np.pi/2.; p4=np.pi/4.
 posxy = lambda x,y: {"x":x, "y":y}
@@ -11,25 +13,24 @@ generators = [lambda x,y: {"type":"coupler", "pos": posxy(x,y), "ratio":0.5},
               lambda x,y: {"type":"phaseshifter", "pos": posxy(x,y), "phase":p4},
               lambda x,y: {"type":"crossing", "pos": posxy(x,y)}]
 
-def base_circuit():
-    return [{"type": "bellpair", "pos": posxy(-1,0)}]
+def sketch(circuit):
+    s="var myCircuitJSON="+json.dumps(circuit)
+    f=open("viewer/scripts/mycircuit.js", "w")
+    f.write(s)
+    f.close()
 
-# TODO: ensure that we make valid circuits
-def fitness(dna):
-    circuit=base_circuit()
-    target=defaultdict(complex, {(0,2):lo.ir2, (1,3):lo.ir2})
-    h,w=dna.shape
-    for i in range(w):
-        for j in range(h):
-            if dna[j,i]>=0: circuit.append(generators[dna[j,i]](i,j*2))
-    circuit=lo.compile_circuit(circuit)
-    output_state = lo.simulate(**circuit)
-    return lo.dinner(output_state, target)
+def get_output(sources, dna):
+    circuit=lo.compile_circuit(sources+dna)
+    circuit.patterns=patterns
+    return lo.simulate(**circuit)
 
+bp = lambda y: {"type": "bellpair", "pos": posxy(-1, y)}
+sps = lambda y: {"type": "sps", "pos": posxy(-1, y)}
+bucket = lambda y: {"type": "bucket", "pos": posxy(100, y)}
+sources=[sps(y) for y in range(10)]
+detectors=[bucket(y) for y in range(2)]
 
-height=10
-width=20
-dna=np.random.randint(0, 5, (height, width))
-dna=np.ones((height, width))*-1
+dna=[generators[0](2,2)]
+sketch(sources+dna)
 
-print fitness(dna)
+#print get_output(sources, dna, detectors)
--- makefile
+++
@@ -1,9 +0,0 @@
-py_target=main
-
-all: py clean
-
-py:
-	python $(py_target).py
-
-clean:
-	@rm -f *.pyc
